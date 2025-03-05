1726a34585459c8d94157f6482783292254c9ed8
SirMphuthi <kgotsomphuthi@gmail.com>
2025-03-05 00:11:57 -0800

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Queries Reddit API and returns total subscribers.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Function that queries the Reddit API.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
+
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
+        return 0

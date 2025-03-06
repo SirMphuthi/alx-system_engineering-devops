23fb0d64b985ce209bde64457d58303597dfd351
SirMphuthi <kgotsomphuthi@gmail.com>
2025-03-06 14:50:00 +0000

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -6,17 +6,3 @@ exec {'modify max open files limit setting':
 }
 
 
-1-user_limit.pp
-
-# Reconfigure the OS for 'holberton' to login and open a file without any error message
-
-exec { 'increase-hard-file-limit-holberton-user':
-  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
-}
-
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
-}
-
